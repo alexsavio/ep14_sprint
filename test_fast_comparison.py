7f46a17797255158c9a178655417678e3509bc05
Borja Ayerdi <ayerdi.borja@gmail.com>
2014-07-26 10:05:39 +0200

    Some small changes.

--- test_fast_comparison.py
+++ test_fast_comparison.py
@@ -1,4 +1,26 @@
+def test_DicomFileDistance():
 
+    from fast_comparison import DicomFileDistance, DicomFilesClustering, group_files
+    from os.path import abspath, dirname, join
+
+    datadir = join(abspath(dirname(__file__)), 'dicoms')
+    file1 = join(datadir, 'subj1_01.dcm')
+    file2 = join(datadir, 'subj2_02.dcm')
+    file3 = join(datadir, 'subj3_01.dcm')
+    file4 = join(datadir, 'subj3_02.dcm')
+    file5 = join(datadir, 'subj3_03.dcm')
+    file6 = join(datadir, 'subj3_04.dcm')
+
+    dist = DicomFileDistance()
+    # Metemos todos los ficheros que tengamos en una 
+    dcm_files = [file1, file2, file3, file4, file5, file6]
+    for i in range(5):
+        dcm_files.extend(dcm_files)
+    #print len(dcm_files)
+    
+    
+    groups = group_files(dcm_files)
+    return groups
 
 def test_SimpleDicomFileDistance():
 
@@ -16,15 +38,13 @@ def test_SimpleDicomFileDistance():
     dist = SimpleDicomFileDistance()
     # Metemos todos los ficheros que tengamos en una 
     dcm_files = [file1, file2, file3, file4, file5, file6]
+    for i in range(5):
+        dcm_files.extend(dcm_files)
+    #print len(dcm_files)
 
     groups = group_files(dcm_files)
     return groups
 
-    #dcmclus = DicomFilesClustering(groups_leaders)
-    #dcmclus._calculate_file_distances()
-    #return dcmclus
-
 if __name__ == '__main__':
     groups = test_SimpleDicomFileDistance()
-    #print groups
-
+    #groups = test_DicomFileDistance()
